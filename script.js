861faffd377639f385e915141460686942dbad14
debasis das <debasis506@gmail.com>
2023-10-09 13:15:27 +0530

    Portfolio Update

--- script.js
+++ script.js
@@ -1,82 +1,76 @@
-
-
-$(document).ready(function () {
-    $(window).scroll(function () {
+$(document).ready(function(){
+    $(window).scroll(function(){
         // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
         }
-
+        
         // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $(".scroll-up-btn").addClass("show");
-        } else {
-            $(".scroll-up-btn").removeClass("show");
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $(".scroll-up-btn").click(function () {
-        $("html").animate({ scrollTop: 0 });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
-        $("html").css("scrollBehavior", "auto");
+        $('html').css("scrollBehavior", "auto");
     });
 
-    $(".navbar .menu li a").click(function () {
+    $('.navbar .menu li a').click(function(){
         // applying again smooth scroll on menu items click
-        $("html").css("scrollBehavior", "smooth");
+        $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $(".menu-btn").click(function () {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Java-Backend-Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Java-Backend-Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
-    $(".carousel").owlCarousel({
+    $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0: {
+            0:{
                 items: 1,
-                nav: false,
+                nav: false
             },
-            600: {
+            600:{
                 items: 2,
-                nav: false,
+                nav: false
             },
-            1000: {
+            1000:{
                 items: 3,
-                nav: false,
-            },
-        },
+                nav: false
+            }
+        }
     });
 });
-
-//change the file here
-document.getElementById("resume-button-1").onclick = () => {
-    window.open(
-        "https://drive.google.com/file/d/1VOOf1p7ezcdcgDFy8cNwHsIf6-fTmg3p/view?usp=sharing",
-        '_blank'
-    );
-};
-
-document.getElementById("resume-button-2").onclick = () => {
-    window.open(
-        "https://drive.google.com/file/d/1VOOf1p7ezcdcgDFy8cNwHsIf6-fTmg3p/view?usp=sharing",
-        '_blank'
-    );
-};
-
-
-GitHubCalendar(".calendar", "dcs1997", {
-    responsive: true,
-    global_stats: true,
-    tooltips: true,
-});
+function resume() {
+    window.open("https://drive.google.com/file/d/1VOOf1p7ezcdcgDFy8cNwHsIf6-fTmg3p/view?usp=sharing");
+}
